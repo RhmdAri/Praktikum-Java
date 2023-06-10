3fe3596a76713baf8fdb46cb6d69408294e7d981
RhmdAri <rhmdari1907@gmail.com>
2023-06-10 20:34:49 +0800

    first commit

---
+++ src/main/java/db/Koneksi.java
@@ -0,0 +1,33 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Koneksi {
+
+    private final String URL = "jdbc:mysql://localhost:3306/db_perpus";
+    private final String USER = "root";
+    private final String PASS = "";
+    
+    public Connection getConnection(){
+        Connection con;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Koneksi Berhasil");
+            
+            return con;
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println( "Koneksi Gagal");
+            return null;
+        }
+            
+    }
+
+    public static void main(String[] args) {
+        Koneksi koneksi = new Koneksi();
+        koneksi.getConnection();
+    }
+}
+
